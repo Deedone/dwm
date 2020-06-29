97a310c3e07359802748af5732066fd6d9365a6d
Mykyta Poturai <mykyta.poturai@gmail.com>
2020-06-29 11:55:16 +0300

    Emoji patch

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const int showsystray        = 1;     /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = {"monospace:size=12", "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
